1b50e70d277deb4503730236937e29680f2e3a7f
hudra0 <117863474+hudra0@users.noreply.github.com>
2024-08-05 01:25:59 +0200

    Add Status/Start/Stop/Restart and Auto-setup

--- htdocs/luci-static/resources/view/settings.js
+++ htdocs/luci-static/resources/view/settings.js
@@ -98,6 +98,140 @@ return view.extend({
         s = m.section(form.NamedSection, 'global', 'global', _('Global Settings'));
         s.anonymous = true;
 
+        o = s.option(form.DummyValue, '_status', _('Service Status'));
+        o.rawhtml = true;
+        o.render = function(section_id) {
+            var status = uci.get('qosmate', 'global', 'enabled') === '1' ? 'Running' : 'Stopped (Disabled)';
+            return E('div', { 'class': 'cbi-value' }, [
+                E('label', { 'class': 'cbi-value-title' }, _('Service Status')),
+                E('div', { 'class': 'cbi-value-field' }, status)
+            ]);
+        };
+        
+        o = s.option(form.DummyValue, '_buttons', _('Service Control'));
+        o.rawhtml = true;
+        o.render = function(section_id) {
+            var buttonStyle = 'button cbi-button';
+            return E('div', { 'class': 'cbi-value' }, [
+                E('label', { 'class': 'cbi-value-title' }, _('Service Control')),
+                E('div', { 'class': 'cbi-value-field' }, [
+                    E('button', {
+                        'class': buttonStyle + ' cbi-button-apply',
+                        'click': ui.createHandlerFn(this, function() {
+                            return fs.exec_direct('/etc/init.d/qosmate', ['start'])
+                                .then(function() { ui.addNotification(null, E('p', _('QoSmate started')), 'success'); })
+                                .catch(function(e) { ui.addNotification(null, E('p', _('Failed to start QoSmate: ') + e), 'error'); });
+                        })
+                    }, _('Start')),
+                    ' ',
+                    E('button', {
+                        'class': buttonStyle + ' cbi-button-neutral',
+                        'click': ui.createHandlerFn(this, function() {
+                            return fs.exec_direct('/etc/init.d/qosmate', ['restart'])
+                                .then(function() { ui.addNotification(null, E('p', _('QoSmate restarted')), 'success'); })
+                                .catch(function(e) { ui.addNotification(null, E('p', _('Failed to restart QoSmate: ') + e), 'error'); });
+                        })
+                    }, _('Restart')),
+                    ' ',
+                    E('button', {
+                        'class': buttonStyle + ' cbi-button-reset',
+                        'click': ui.createHandlerFn(this, function() {
+                            return fs.exec_direct('/etc/init.d/qosmate', ['stop'])
+                                .then(function() { ui.addNotification(null, E('p', _('QoSmate stopped')), 'success'); })
+                                .catch(function(e) { ui.addNotification(null, E('p', _('Failed to stop QoSmate: ') + e), 'error'); });
+                        })
+                    }, _('Stop'))
+                ])
+            ]);
+        };
+
+        // Auto Setup Button
+        o = s.option(form.Button, '_auto_setup', _('Auto Setup'));
+        o.inputstyle = 'apply';
+        o.inputtitle = _('Start Auto Setup');
+        o.onclick = ui.createHandlerFn(this, function() {
+            ui.showModal(_('Auto Setup'), [
+                E('p', _('This will run a speed test and configure QoSmate automatically.')),
+                E('div', { 'class': 'cbi-value' }, [
+                    E('label', { 'class': 'cbi-value-title' }, _('Gaming Device IP (optional)')),
+                    E('input', { 'id': 'gaming_ip', 'type': 'text', 'class': 'cbi-input-text' })
+                ]),
+                E('div', { 'class': 'right' }, [
+                    E('button', {
+                        'class': 'btn',
+                        'click': ui.hideModal
+                    }, _('Cancel')),
+                    ' ',
+                    E('button', {
+                        'class': 'btn cbi-button-action',
+                        'click': ui.createHandlerFn(this, function() {
+                            var gamingIp = document.getElementById('gaming_ip').value;
+                            ui.showModal(_('Running Auto Setup'), [
+                                E('p', { 'class': 'spinning' }, _('Please wait while the auto setup is in progress...'))
+                            ]);
+                            return fs.exec_direct('/etc/init.d/qosmate', ['auto_setup_noninteractive', gamingIp])
+                                .then(function(res) {
+                                    var outputFile = res.trim();
+                                    return fs.read(outputFile).then(function(output) {
+                                        ui.hideModal();
+                                        
+                                        // Extrahiere relevante Informationen
+                                        var wanInterface = output.match(/Detected WAN interface: (.+)/);
+                                        var downloadSpeed = output.match(/Download speed: (.+) Mbit\/s/);
+                                        var uploadSpeed = output.match(/Upload speed: (.+) Mbit\/s/);
+                                        var downrate = output.match(/DOWNRATE: (.+) kbps/);
+                                        var uprate = output.match(/UPRATE: (.+) kbps/);
+                                        var gamingRules = output.match(/Gaming device rules added for IP: (.+)/);
+        
+                                        ui.showModal(_('Auto Setup Results'), [
+                                            E('h4', _('Speed Test Results')),
+                                            E('div', { 'class': 'cbi-value' }, [
+                                                E('label', { 'class': 'cbi-value-title' }, _('WAN Interface')),
+                                                E('div', { 'class': 'cbi-value-field' }, wanInterface ? wanInterface[1] : _('Not detected'))
+                                            ]),
+                                            E('div', { 'class': 'cbi-value' }, [
+                                                E('label', { 'class': 'cbi-value-title' }, _('Download Speed')),
+                                                E('div', { 'class': 'cbi-value-field' }, downloadSpeed ? downloadSpeed[1] + ' Mbit/s' : _('Not available'))
+                                            ]),
+                                            E('div', { 'class': 'cbi-value' }, [
+                                                E('label', { 'class': 'cbi-value-title' }, _('Upload Speed')),
+                                                E('div', { 'class': 'cbi-value-field' }, uploadSpeed ? uploadSpeed[1] + ' Mbit/s' : _('Not available'))
+                                            ]),
+                                            E('h4', _('QoS Configuration')),
+                                            E('div', { 'class': 'cbi-value' }, [
+                                                E('label', { 'class': 'cbi-value-title' }, _('Download Rate')),
+                                                E('div', { 'class': 'cbi-value-field' }, downrate ? downrate[1] + ' kbps' : _('Not set'))
+                                            ]),
+                                            E('div', { 'class': 'cbi-value' }, [
+                                                E('label', { 'class': 'cbi-value-title' }, _('Upload Rate')),
+                                                E('div', { 'class': 'cbi-value-field' }, uprate ? uprate[1] + ' kbps' : _('Not set'))
+                                            ]),
+                                            gamingRules ? E('div', { 'class': 'cbi-value' }, [
+                                                E('label', { 'class': 'cbi-value-title' }, _('Gaming Rules')),
+                                                E('div', { 'class': 'cbi-value-field' }, _('Added for IP: ') + gamingRules[1])
+                                            ]) : '',
+                                            E('div', { 'class': 'right' }, [
+                                                E('button', {
+                                                    'class': 'btn cbi-button-action',
+                                                    'click': ui.createHandlerFn(this, function() {
+                                                        ui.hideModal();
+                                                        location.reload();
+                                                    })
+                                                }, _('Apply and Reload'))
+                                            ])
+                                        ]);
+                                    });
+                                })
+                                .catch(function(err) {
+                                    ui.hideModal();
+                                    ui.addNotification(null, E('p', _('Auto setup failed: ') + err), 'error');
+                                });
+                        })
+                    }, _('Start'))
+                ])
+            ]);
+        });
+
         o = s.option(form.Flag, 'enabled', _('Enable'), _('Enable or disable qosmate'));
         o.rmempty = false;
 
